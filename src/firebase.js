c08e1175e1d2a131137f19d1ea0713e81f66c678
Oleksii Koshelenko <oleksii_koshelenko@epam.com>
2024-06-23 20:57:06 +0300

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAPNasTDuiZ0VAmK888CM9pXBZi0lus_hc",
+  authDomain: "ddive-c5c2a.firebaseapp.com",
+  projectId: "ddive-c5c2a",
+  storageBucket: "ddive-c5c2a.appspot.com",
+  messagingSenderId: "1022483861692",
+  appId: "1:1022483861692:web:bc7431a9a2b54e416040b9"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -7,15 +7,28 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import HttpBackend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import { initializeApp } from 'firebase/app';
 
+// Импорт Firebase конфигурации
+const firebaseConfig = {
+  apiKey: "AIzaSyAPNasTDuiZ0VAmK888CM9pXBZi0lus_hc",
+  authDomain: "ddive-c5c2a.firebaseapp.com",
+  projectId: "ddive-c5c2a",
+  storageBucket: "ddive-c5c2a.appspot.com",
+  messagingSenderId: "1022483861692",
+  appId: "1:1022483861692:web:bc7431a9a2b54e416040b9"
+};
+
+// Инициализация Firebase
+const firebaseApp = initializeApp(firebaseConfig);
 
 i18n
-  .use(HttpBackend) 
+  .use(HttpBackend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: 'en',
-    debug: true, 
+    debug: true,
     detection: {
       order: ['queryString', 'cookie', 'localStorage', 'path', 'subdomain'],
       caches: ['cookie'],
@@ -25,13 +38,13 @@ i18n
     },
   });
 
-  ReactDOM.render(
-    <React.StrictMode>
-      <Suspense fallback={<div>Loading...</div>}>
-        <App />
-      </Suspense>
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-  
-  reportWebVitals();
+ReactDOM.render(
+  <React.StrictMode>
+    <Suspense fallback={<div>Loading...</div>}>
+      <App />
+    </Suspense>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
